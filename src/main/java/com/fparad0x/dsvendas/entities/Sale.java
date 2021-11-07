49f7638051710061db56c6a4b249a949f3608857
Erick Cesar <e.cesar88@gmail.com>
2021-11-07 12:11:10 -0400

    Db seed, entity mapping

---
+++ src/main/java/com/fparad0x/dsvendas/entities/Sale.java
@@ -0,0 +1,35 @@
+package com.fparad0x.dsvendas.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+}
---
+++ src/main/java/com/fparad0x/dsvendas/entities/Seller.java
@@ -0,0 +1,32 @@
+package com.fparad0x.dsvendas.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "seller")
+    private @Getter List<Sale> sale = new ArrayList<>();
+}
